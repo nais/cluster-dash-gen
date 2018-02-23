184927fcf954f3c256612160d9b3f6bb139aedbd
Castigere <even.haasted@nav.no>
2018-02-23 16:32:13 +0100

    ...

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // const generateDashboard = require('./js/generator')
-const program = require('commander')
-const templater = require('json-templater/object')
+// const program = require('commander')
+// const templater = require('json-templater/object')
 // const util = require('./js/util')
+console.log('what')
 
+// function list(val) {
+//     return val.split(',')
+// }
 
-function list(val) {
-    return val.split(',')
-}
+// program
+//     .usage('[options] <args>')
+//     .option('-c, --cluster <cluster name>', 'Cluster name')
+//     .option('-m, --masters <comma separated host list>', 'all master nodes in the cluster', list)
+//     .option('-w, --workers <comma separated host list>', 'all worker nodes in the cluster', list)
+//     .option('-t, --template <file.json>', 'JSON template for the dashboard')
+//     .parse(process.argv)
 
-program
-    .usage('[options] <args>')
-    .option('-c, --cluster <cluster name>', 'Cluster name')
-    .option('-m, --masters <comma separated host list>', 'all master nodes in the cluster', list)
-    .option('-w, --workers <comma separated host list>', 'all worker nodes in the cluster', list)
-    .option('-t, --template <file.json>', 'JSON template for the dashboard')
-    .parse(process.argv)
-
-console.log(' - Cluster name: %s', program.cluster)
-console.log(' - Master nodes: %j', program.masters)
-console.log(' - Worker nodes: %j', program.workers)
-console.log(' - Template: %s', program.template)
+// console.log(' - Cluster name: %s', program.cluster)
+// console.log(' - Master nodes: %j', program.masters)
+// console.log(' - Worker nodes: %j', program.workers)
+// console.log(' - Template: %s', program.template)
 
 // if (!program.cluster || !program.masters || !program.workers || !program.template) {
 //     console.log('Argument(s) missing, use --help for more info')
